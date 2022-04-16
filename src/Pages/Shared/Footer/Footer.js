fbc9b7c210825a4694873c5b20288da5bc13f983
Your Name <you@example.com>
2022-04-16 12:46:02 -0700

    add footer

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -2,9 +2,10 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe fugiat
-      temporibus at totam id! Quis laborum eveniet sapiente nostrum illum.
+    <div className="bg-dark text-white mb-0 p-4 d-flex justify-content-center align-items-center">
+      <div className="">
+        <p>&copy; Travel World, 2022</p>
+      </div>
     </div>
   );
 };
