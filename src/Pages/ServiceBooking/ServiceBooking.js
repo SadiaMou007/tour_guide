234dd185597cc498caf74e6de8077af85f91925c
Your Name <you@example.com>
2022-04-17 12:50:30 -0700

    update profile and email verification add

--- src/Pages/ServiceBooking/ServiceBooking.js
+++ src/Pages/ServiceBooking/ServiceBooking.js
@@ -5,9 +5,37 @@ const ServiceBooking = () => {
   const { serviceId } = useParams();
 
   return (
-    <div>
-      Id:
-      {serviceId}
+    <div className="w-50 mx-auto vh-100 my-4">
+      <div className="border mt-3 p-4">
+        <h2 className="text-info text-center my-3"> Checkout</h2>
+        <form>
+          <h3>Service Id: {serviceId}</h3>
+          <div>
+            <input
+              type="text"
+              placeholder="Enter your name"
+              className="my-2 p-2 w-100"
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              placeholder="Address"
+              className="my-2 p-4 w-100"
+            />
+          </div>
+          <div>
+            <input
+              type="number"
+              placeholder="Phone number"
+              className="my-2 p-2 w-100"
+            />
+          </div>
+          <button className="border-0 p-2 my-2 text-white bg-info">
+            Proceed Checkout
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,32 +1,45 @@
 import { React, useState } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import {
+  useCreateUserWithEmailAndPassword,
+  useUpdateProfile,
+} from "react-firebase-hooks/auth";
 import auth from "../../Firebase/firebase.init.js";
 
 import "./SignUp.css";
 import SocialLogin from "../Login/SocialLogin/SocialLogin.js";
+
 const SignUp = () => {
+  const [agree, setAgree] = useState(false);
+
   const navigate = useNavigate();
   const [createUserWithEmailAndPassword, user, loading, error] =
-    useCreateUserWithEmailAndPassword(auth);
+    useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
+
+  const [updateProfile, updating, updateError] = useUpdateProfile(auth);
 
   const [email, setEmail] = useState({ value: "", error: "" });
   const [password, setPassword] = useState({ value: "", error: "" });
   const [cpassword, setCpassword] = useState({ value: "", error: "" });
 
-  const handleSignUp = (e) => {
+  const handleSignUp = async (e) => {
     e.preventDefault();
     const email = e.target.email.value;
     const name = e.target.name.value;
     const pass = e.target.password.value;
     const cpass = e.target.confirmPassword.value;
-    createUserWithEmailAndPassword(email, pass);
-    console.log(name, email, pass, cpass);
-  };
-  if (user) {
+
+    await createUserWithEmailAndPassword(email, pass);
+    await updateProfile({ displayName: name });
+    console.log("update user profile");
     navigate("/home");
+  };
+
+  if (user) {
+    console.log("user", user);
   }
+
   return (
     <div className=" w-50  container-fluid signup-container my-2 s-container">
       <div className="login-c">
@@ -84,15 +97,22 @@ const SignUp = () => {
           </div>
           <div>
             <input
+              onClick={() => setAgree(!agree)}
               type="checkbox"
               name="terms"
               id="terms"
               className="p-2 ms-3 me-2"
             />
-            <label htmlFor="terms">Accept our terms and conditions? </label>
+            <label
+              htmlFor="terms"
+              className={agree ? "text-success" : "text-danger"}
+            >
+              Accept our terms and conditions?{" "}
+            </label>
           </div>
           <div className="p-3">
             <button
+              disabled={!agree}
               type="submit"
               className="w-100 p-2 rounded border-0 bg-success text-white bg-opacity-50"
             >
